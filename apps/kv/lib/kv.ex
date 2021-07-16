6b6d960943d8974e88924e435f54b72811e62d2c
jwandekoken <julio.wandekoken@paketa.com.br>
2021-07-16 08:00:09 -0300

    first commit

---
+++ apps/kv/lib/kv.ex
@@ -0,0 +1,10 @@
+defmodule KV do
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    # Although we don't use the supervisor name below directly,
+    # it can be useful when debugging or introspecting the system.
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
+end
---
+++ apps/kv/test/bucket_test.exs
@@ -0,0 +1,25 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    bucket = start_supervised!(KV.Bucket)
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "delete a value by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.delete(bucket, "milk") == 3
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+
+  test "are temporary workers" do
+    assert Supervisor.child_spec(KV.Bucket, []).restart == :temporary
+  end
+end
